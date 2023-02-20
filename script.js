25fe606c64cbad922f355b4546b1de43462e13b5
pathymavuba <mavubapathy@gmail.com>
2023-02-20 17:37:36 +0100

    algo LongestWord with typescript

--- script.js
+++ script.js
@@ -69,3 +69,23 @@ function valMinMax(array){
       return len;
       
   };
+
+  function LongestWord(sen) {
+   let result = "";
+   sen = sen.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+   const array = sen.split(" ");
+   for (let i = 0; i < array.length - 1; i++) {
+       if (array[i].length >= array[i + 1].length) {
+           result = array[i];
+       }
+       else {
+           result = array[i + 1];
+       }
+   }
+   // code goes here  
+   return result;
+}
+// keep this function call here 
+// @ts-ignore
+console.log(LongestWord("fun&!! time"));
+
